df4cc2ad3af450e45e4a89d5c0ee483aec1896dd
Collins Tatang <makungongcollins@gmail.com>
2023-05-11 15:37:10 +0100

    Update addData file

--- addData.js
+++ addData.js
@@ -2,31 +2,40 @@ import inquirer from "inquirer";
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 import queryDB from "./queryDB.js";
-import { error } from "console";
 
 export default async function addData(info) {
     try {
       const answer = await inquirer.prompt([
-            { type: "input", name: "name", massage: "Please Enter Your name" },
-            { type: "number", name: "phone", massage: "Please Enter Your phone" },
             { 
-              type: "list",
-              name: "age", 
-              massage: "Please are you an adult", 
-              choices: [
-                {name: "Y", value: "Adult"},
-                {name: "N", value: "Minor"}
-              ],
+                type: "input", 
+                name: "Name", 
+                massage: "Please Enter Your name" 
+            },
+            { 
+                type: "number", 
+                name: "Phone", 
+                massage: "Please Enter Your phone" 
+            },
+            { 
+                type: "list",
+                name: "Are you an adult", 
+                massage: "Please are you an adult", 
+                choices: [
+                    {name: "Y", value: "Adult"},
+                    {name: "N", value: "Minor"}
+                ],
             },
         ]);
 
+        //Assign ID's and set of input
         const data = {
-            id: uuidv4(),
+           id: uuidv4(),
            Name: answer.name,
            Phone: answer.phone,
            Age: answer.age
         };
 
+        //Push everything to the info array
         info.push(data);
 
         if (fs.existsSync("db.json file created successfully")) {
